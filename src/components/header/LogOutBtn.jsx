9fa4118a3dd064b197cb2689c3c088e4a8c79f07
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-15 18:35:19 +0530

    started building components

---
+++ src/components/header/LogOutBtn.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authservice from '../../appwrite/auth'
+import {logout} from "../../store/authSlice"
+
+function LogOutBtn() {
+    const dispatch = useDispatch();
+    
+    const logoutHandler = () => {
+        authservice.logOut().then(() => {
+            dispatch(logout());
+        }).catch(error => {
+            console.error("Logout failed:", error);
+        });
+    };
+  return (
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
+  )
+}
+
+export default LogOutBtn
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,12 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Contanier from './contanier/Contanier'
+import Logo from "./Logo";
+import LogOutBtn from "./header/LogOutBtn"
+
 export {
     Header,
-    Footer
+    Footer,
+    Contanier,
+    Logo
 }
