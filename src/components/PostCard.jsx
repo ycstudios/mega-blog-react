70aeedbf802ca31edef579b9020910d704b87e86
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-16 10:57:20 +0530

    react hook form build

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import appwrtieService from "../appwrite/conf"
+
+
+
+function PostCard({$id,title,featuredImage}) {
+
+  return (
+    <Link to={`/post/${$id}`}>
+           <div className='w-full bg-gray-100 rounded-xl p-4'>
+           <div className='w-full justify-center mb-4'>
+
+           <img src={appwrtieService.getfilePrevew(featuredImage)} alt={title}
+                className='rounded-xl' />
+
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+            </div>
+    </Link>
+  )
+}
+
+export default PostCard
