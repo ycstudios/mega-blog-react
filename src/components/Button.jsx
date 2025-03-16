ff387fd0155ab6239f33730e6e85315e6e4907c8
Yash Chandanshive <yashchandanshive18@gmail.com>
2025-03-16 15:33:02 +0530

    fixed error

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,5 +12,4 @@ export default function Button({
         <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
             {children}
         </button>
-    );
-}
+    );1}
